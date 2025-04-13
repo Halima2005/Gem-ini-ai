3d6b2ff0c2603993eb07d3c40dedb632a03953db
Halima2005 <150766132+Halima2005@users.noreply.github.com>
2025-04-13 10:30:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Main from "./components/Main/Main";
 
-import Sidebar from "./components/sidebar/sidebar";
+import Sidebar from "./components/Sidebar/Sidebar";
+
 const App = () => {
   return (
     <>
